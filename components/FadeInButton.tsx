1e86071401411fd62345558b3fd99ab9991c458c
Hrithik-22 <kedarehrithik@gmail.com>
2025-01-05 11:55:16 +0530

    withRepeat

--- components/FadeInButton.tsx
+++ components/FadeInButton.tsx
@@ -1,9 +1,9 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Easing } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import Animated, { runOnJS, runOnUI, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import Animated, { runOnJS, runOnUI, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated'
 
 const FadeInButton: () => React.JSX.Element = () => {
-    const [shouldMount, setShouldMount] = useState(true)
+    // const [shouldMount, setShouldMount] = useState(true)
     const opacity = useSharedValue(0);
 
     const animatedStyle = useAnimatedStyle(() => {
@@ -11,29 +11,33 @@ const FadeInButton: () => React.JSX.Element = () => {
             opacity: opacity.value,
         }
     }, []);
-    const unMountCircle = () => {
-        setShouldMount(false);
+    // const unMountCircle = () => {
+    //     setShouldMount(false);
+    // }
+    const fadeIn = () => {
+        'worklet';
+        opacity.value = withRepeat(withTiming(1, { duration: 2000 }), 5, false)
     }
+    // const fadeIn = () => {
+    //     'worklet';
+    //     opacity.value =
+    //             withTiming(1, { duration: 300 }, (finished) => {
+    //                 //when need something from the thread
+    //                 //Brings data or actions back to the JavaScript thread.
+    //                 finished && runOnJS(unMountCircle)()
+    //             })
+    // }
     useEffect(() => {
-        const fadeIn = () => {
-            'worklet';
-            opacity.value = withTiming(1, { duration: 3000 }, (finished) => {
-                //when need something from the thread
-                //Brings data or actions back to the JavaScript thread.
-                finished && runOnJS(unMountCircle)()
-            })
-        }
         // run on the ui thread
         //need 'worklet'makes it workletized function
         runOnUI(fadeIn)()
     }, [])
     return (
         <View style={styles.container}>
-            {
-                shouldMount &&
-                <Animated.View style={[styles.circle, animatedStyle]} />
 
-            }
+            <Animated.View style={[styles.circle, animatedStyle]} />
+
+
         </View>
     )
 }
